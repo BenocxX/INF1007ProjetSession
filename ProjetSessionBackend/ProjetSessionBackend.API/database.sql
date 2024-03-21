d9c9e859fcd30355a2bc47141e757d7cbfce11d0
Samuel Tessier <72167889+ZElGHTS@users.noreply.github.com>
2024-03-21 19:45:09 -0400

    Feat : Finalize V1 of database script

--- ProjetSessionBackend/ProjetSessionBackend.API/database.sql
+++ ProjetSessionBackend/ProjetSessionBackend.API/database.sql
@@ -3,6 +3,9 @@ CREATE SCHEMA IF NOT EXISTS project;
 
 SET SEARCH_PATH TO project;
 
+\ir Sql/security.sql;
 \ir Sql/tables.sql;
+\ir Sql/Functions/person.sql;
+\ir Sql/Functions/client.sql;
 
 set session "project.encryption_key" = 'yaxN62KQXT7snMnALpbEsd/eMxAlnT2gUI5WNxTfX0hbs4bkXir/Jv2sIjYlrR/MaduntmR/mVKtNDOkrOcUIw==';
