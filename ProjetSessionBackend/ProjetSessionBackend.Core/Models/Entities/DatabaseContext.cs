159e9f41a595bd39ec2963b35b7199e3055e71e3
BenocxX <mathis.cote@outlook.com>
2024-03-27 14:56:51 -0400

    fix: update docker container to work with multiple c# projects

--- ProjetSessionBackend/ProjetSessionBackend.Core/Models/Entities/DatabaseContext.cs
+++ ProjetSessionBackend/ProjetSessionBackend.Core/Models/Entities/DatabaseContext.cs
@@ -15,23 +15,12 @@ public partial class DatabaseContext : DbContext
     {
     }
 
-    public virtual DbSet<User> Users { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Database=projet-session;\nUsername=dev;Password=dev");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity
-                .HasNoKey()
-                .ToTable("user");
-
-            entity.Property(e => e.Name).HasColumnName("name");
-        });
-
         OnModelCreatingPartial(modelBuilder);
     }
 
