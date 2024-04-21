b449494d10df3b8c4ce3de913bf0f8c2624a6a80
MathyasLefebvre <mathylefebvre@hotmail.com>
2024-04-21 19:03:10 -0400

    ✨Create CRUD pages for the menuItem

--- projet-session-frontend/src/routes/menuItem/$menuItemId/edit.tsx
+++ projet-session-frontend/src/routes/menuItem/$menuItemId/edit.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute, useParams } from "@tanstack/react-router";
+import { Link, createFileRoute, useParams } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { fetchMenuItemById, updateMenuItem } from "../../../api/menuItems";
 import Input from "../../../components/form/input";
-import { object, string, number } from "yup";
+import { object, string, number, boolean } from "yup";
 
 export const Route = createFileRoute("/menuItem/$menuItemId/edit")({
   component: Edit,
@@ -10,21 +10,25 @@ export const Route = createFileRoute("/menuItem/$menuItemId/edit")({
 });
 
 function Edit() {
-  const { menuId } = useParams({ strict: false });
+  const { menuItemId } = useParams({ strict: false });
+  let [id, setId] = useState(menuItemId);
   let [name, setName] = useState("");
   let [description, setDescription] = useState("");
   let [price, setPrice] = useState("");
+  let [available, setAvailable] = useState(false);
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
-    const id = parseInt(menuId, 10);
+    setId(parseInt(menuItemId, 10));
+
     fetchItem(id);
-  }, [menuId]);
+  }, [menuItemId]);
 
   const validationSchema = object().shape({
     name: string().required(),
     description: string().required(),
     price: number().required(),
+    available: boolean().required(),
   });
 
   const fetchItem = async (id: number) => {
@@ -32,6 +36,7 @@ function Edit() {
     setName(data.name);
     setDescription(data.description);
     setPrice(data.price);
+    setAvailable(data.available);
   };
 
   const handleSubmit = async (e: any) => {
@@ -41,10 +46,13 @@ function Edit() {
       name: name,
       description: description,
       price: price,
+      available: available,
     };
 
     try {
       await validationSchema.validate(formData, { abortEarly: false });
+      const response = await updateMenuItem(id, formData);
+      console.log(response);
     } catch (validationErrors: any) {
       const formattedErrors: Array<any> = [];
       validationErrors.inner.forEach((error: any) => {
@@ -54,44 +62,73 @@ function Edit() {
     }
 
     if (errors.length == 0) {
-      await updateMenuItem(parseInt(menuId, 10), formData);
+      await updateMenuItem(parseInt(menuItemId, 10), formData);
     }
   };
 
+  const handleToggleChange = (e: any) => {
+    setAvailable(e.target.checked);
+  };
+
   return (
-    <div>
-      <h1 className="text-center text-3xl">Ajouter un plat</h1>
+    <div className="bg-white rounded-lg shadow-md p-6 max-w-md mx-auto">
+      <h1 className="text-center text-3xl mb-4">Ajouter un plat</h1>
       <form
         method="put"
-        action={import.meta.env.BASE_URL + "/menu/" + menuId}
+        action={import.meta.env.BASE_URL + "/menu/" + menuItemId}
         onSubmit={handleSubmit}
       >
-        <Input
-          name="name"
-          placeholder="Nom du plat"
-          value={name}
-          onChange={setName}
-          errors={errors}
-        />
-        <Input
-          name="description"
-          placeholder="Description"
-          value={description}
-          onChange={setDescription}
-          errors={errors}
-        />
-        <Input
-          name="price"
-          placeholder="Prix"
-          type="number"
-          value={price}
-          onChange={setPrice}
-          errors={errors}
-        />
-
-        <button type="submit" className="btn btn-primary">
-          Soummettre
-        </button>
+        <div className="mb-4">
+          <Input
+            name="name"
+            placeholder="Nom du plat"
+            value={name}
+            onChange={setName}
+            errors={errors}
+          />
+        </div>
+        <div className="mb-4">
+          <Input
+            name="description"
+            placeholder="Description"
+            value={description}
+            onChange={setDescription}
+            errors={errors}
+          />
+        </div>
+        <div className="mb-4">
+          <Input
+            name="price"
+            placeholder="Prix"
+            type="number"
+            value={price}
+            onChange={setPrice}
+            errors={errors}
+          />
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Afficher sur le menu</span>
+            <input
+              type="checkbox"
+              className="toggle"
+              checked={available}
+              onChange={handleToggleChange}
+            />
+          </label>
+        </div>
+        <div className="flex justify-end">
+          <Link to="/menu/create" type="button" className="btn btn-light m-4">
+            Annuler
+          </Link>
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="btn btn-success mt-4 text-white"
+          >
+            Soummettre
+          </button>
+        </div>
       </form>
     </div>
   );
--- projet-session-frontend/src/routes/menuItem/create.tsx
+++ projet-session-frontend/src/routes/menuItem/create.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SetStateAction, useEffect, useState } from "react";
-import { object, string, number } from "yup";
+import { Link, createFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { object, string, number, boolean } from "yup";
 import Input from "../../components/form/input";
 import { addMenuItem } from "../../api/menuItems";
 
@@ -14,6 +14,7 @@ function Create() {
   let [name, setName] = useState("");
   let [description, setDescription] = useState("");
   let [price, setPrice] = useState("");
+  let [available, setAvailable] = useState(false);
   const [errors, setErrors] = useState({});
 
   useEffect(() => {
@@ -24,6 +25,7 @@ function Create() {
     name: string().required(),
     description: string().required(),
     price: number().required(),
+    available: boolean().required(),
   });
 
   const handleSubmit = async (e: any) => {
@@ -33,6 +35,7 @@ function Create() {
       name: name,
       description: description,
       price: price,
+      available: available,
     };
 
     try {
@@ -47,39 +50,65 @@ function Create() {
     }
   };
 
+  const handleToggleChange = (e: any) => {
+    setAvailable(e.target.checked);
+  };
+
   return (
-    <div>
-      <h1 className="text-center text-3xl">Ajouter un plat</h1>
+    <div className="bg-white rounded-lg shadow-md p-6 max-w-md mx-auto">
+      <h1 className="text-center text-3xl mb-4">Ajouter un plat</h1>
       <form
         method="post"
         action={import.meta.env.BASE_URL + "/menu"}
         onSubmit={handleSubmit}
       >
-        <Input
-          name="name"
-          placeholder="Nom du plat"
-          value={name}
-          onChange={setName}
-          errors={errors}
-        />
-        <Input
-          name="description"
-          placeholder="Description"
-          value={description}
-          onChange={setDescription}
-          errors={errors}
-        />
-        <Input
-          name="price"
-          placeholder="Prix"
-          type="number"
-          value={price}
-          onChange={setPrice}
-          errors={errors}
-        />
-        <button type="submit" className="btn btn-primary">
-          Soummettre
-        </button>
+        <div className="mb-4">
+          <Input
+            name="name"
+            placeholder="Nom du plat"
+            value={name}
+            onChange={setName}
+            errors={errors}
+          />
+        </div>
+        <div className="mb-4">
+          <Input
+            name="description"
+            placeholder="Description"
+            value={description}
+            onChange={setDescription}
+            errors={errors}
+          />
+        </div>
+        <div className="mb-4">
+          <Input
+            name="price"
+            placeholder="Prix"
+            type="number"
+            value={price}
+            onChange={setPrice}
+            errors={errors}
+          />
+        </div>
+        <div className="form-control">
+          <label className="label cursor-pointer">
+            <span className="label-text">Afficher sur le menu</span>
+            <input
+              type="checkbox"
+              className="toggle"
+              checked={available}
+              onChange={handleToggleChange}
+            />
+          </label>
+        </div>
+        <div className="flex justify-end">
+          <Link to="/menu/create" type="button" className="btn btn-light m-4">
+            Annuler
+          </Link>
+          <button type="submit" className="btn btn-success mt-4 text-white">
+            Soummettre
+          </button>
+        </div>
       </form>
     </div>
   );
