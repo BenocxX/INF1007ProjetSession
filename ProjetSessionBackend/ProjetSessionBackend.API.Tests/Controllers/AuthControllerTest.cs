323bff93e1d3305b82f177e2ce88a363d475539c
BenocxX <mathis.cote@outlook.com>
2024-04-26 13:19:08 -0400

    tests: make real tests with mock data

--- ProjetSessionBackend/ProjetSessionBackend.API.Tests/Controllers/AuthControllerTest.cs
+++ ProjetSessionBackend/ProjetSessionBackend.API.Tests/Controllers/AuthControllerTest.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using ProjetSessionBackend.API.Controllers;
+using ProjetSessionBackend.Core.Models.DTOs;
 
 namespace ProjetSessionBackend.API.Tests.Controllers;
 
@@ -12,15 +17,39 @@ public class AuthControllerTest
 {
     private const string Url = "http://localhost:5117";
     
+    private HttpClient _http;
+    private JsonSerializerSettings _jsonSettings;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _http = new HttpClient();
+        _jsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy
+                {
+                    ProcessDictionaryKeys = true
+                }
+            }
+        };
+    }
+
     [TestMethod]
-    public async Task POST_Login_Returns_OK()
+    public async Task POST_Login_As_Admin_Returns_OK()
     {
         // Arrange
-        var client = new HttpClient();
-        var request = new HttpRequestMessage(HttpMethod.Post, ApiRoute("/login"));
+        var body = new UserLoginResponse
+        {
+            Email = "admin@example.com",
+            Password = "Omega123"
+        };
+        
+        var request = MakePostRequest("/login", body);
 
         // Act
-        var response = await client.SendAsync(request);
+        var response = await _http.SendAsync(request);
 
         // Assert
         response.EnsureSuccessStatusCode();
@@ -30,15 +59,35 @@ public class AuthControllerTest
     public async Task POST_Register_Returns_OK()
     {
         // Arrange
-        var client = new HttpClient();
-        var request = new HttpRequestMessage(HttpMethod.Post, ApiRoute("/register"));
+        var body = new RegisterResponse
+        {
+            Firstname = "bob",
+            Lastname = "dole",
+            Email = "bob.dole@example.com",
+            Password = "Omega123*"
+        };
+        
+        var request = MakePostRequest("/register", body);
 
         // Act
-        var response = await client.SendAsync(request);
+        var response = await _http.SendAsync(request);
 
         // Assert
         response.EnsureSuccessStatusCode();
     }
     
-    private string ApiRoute(string route) => $"{Url}/{route}";
+    private HttpRequestMessage MakePostRequest(string route, object body = null)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, ApiRoute(route));
+        
+        if (body == null) 
+            return request;
+        
+        var json = JsonConvert.SerializeObject(body, _jsonSettings);
+        request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        return request;
+    }
+    
+    private static string ApiRoute(string route) => $"{Url}{route}";
 }
